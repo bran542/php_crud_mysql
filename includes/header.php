35c1bcbe7e82579c4645c5e28e852954ccc3dbe0
Brandon M <brandonmunoz542@gmail.com>
2022-09-06 02:32:07 -0300

    Se agrego navbar a la pagina web

--- includes/header.php
+++ includes/header.php
@@ -9,3 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
