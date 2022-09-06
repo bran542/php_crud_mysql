63d72f86a92b5d0dd2d39dceac2e046b4acb99df
Brandon M <brandonmunoz542@gmail.com>
2022-09-06 01:30:26 -0300

    Conexion a base de datos

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect(
+    'localhost','root','','php_crud_mysql_db'
+);
+/* PRUBA DE CONEXION A LA BASE DE DATOS
+if(isset($conn)){
+    echo 'DB IS CONNECTED';
+}
+*/
+?>
