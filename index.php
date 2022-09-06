3ee023fd7f7b5c807a7aee13e910986debc24dc5
Brandon M <brandonmunoz542@gmail.com>
2022-09-06 02:56:32 -0300

    Se creo formulario de tareas en index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-echo "<h1>hello world</h1>"
-?>
+<?php include("db.php")?>
+
+<?php include("includes/header.php")?>
+
+<div class="container p-4">
+
+    <div class="row">
+
+        <div class="col-md-4">
+
+            <div class="card card-body">
+                <form action="save.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control"
+                        placeholder="Task Title" autofocus>
+                    </div><br>
+                    <div class="form-group">
+                        <textarea name="description" rows="2" class="form-control"
+                        placeholder="Task Description"></textarea>
+                    </div><br>
+                    <input type="submit" class="btn btn-success btn-block"
+                    name="save_task" value="Save Task">
+                </form>
+            </div>
+
+        </div>
+
+        <div class="col-md-8">
+
+        </div>
+
+    </div>
+</div>
+
+<?php include("includes/footer.php")?>
