72acdfaf57eadaf7cd29cb1d0ef415b75e83281e
Brandon M <brandonmunoz542@gmail.com>
2022-09-06 22:02:35 -0300

    Se creo Consulta para llamar los Datos desde MYSQL y ser imprimidos en una tabla de index.php

--- index.php
+++ index.php
@@ -35,7 +35,39 @@
         </div>
 
         <div class="col-md-8">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Created At</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- SOLICITAR LOS DATOS DESDE MYSQL (CONSULTA) -->
+                    <?php
+                    $query= "SELECT * FROM task";
+                    $result_tasks = mysqli_query($conn, $query);
+                    //RECORREMOS 1 A 1 NUESTRAS TAREAS (REGISTROS)
+                    while($row = mysqli_fetch_array($result_tasks)){ ?>
+                        <tr>
+                            <td><?php echo $row['title'] ?></td>
+                            <td><?php echo $row['description'] ?></td>
+                            <td><?php echo $row['created_at'] ?></td>
+                            <td>
+                                <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
+                                    <i class="fas fa-marker"></i>
+                                </a>
+                                <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                                    <i class="far fa-trash-alt"></i>
+                                </a>
+                            </td>
+                        </tr>
 
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
 
     </div>
